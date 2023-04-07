63f6d02df9912dc95e5f80cb0a255ce2efc7570b
Hrushi M <108924653+superflows-dev@users.noreply.github.com>
2023-04-07 12:01:34 +0530

    Added fields

--- globals.mjs
+++ globals.mjs
@@ -9,12 +9,13 @@ const ddbClient = new DynamoDBClient({ region: REGION });
 const TABLE = "DB_TABLE_NAME";
 const LOG_GROUP_NAME = "AWS_LOG_GROUP_NAME";
 
-const AUTH_ENABLE = AWS_ENABLE_AUTH;
+const AUTH_ENABLE = true;
 const AUTH_REGION = "AWS_AUTH_REGION";
 const AUTH_API = "AWS_AUTH_API";
 const AUTH_STAGE = "test";
 
 const ADMIN_METHODS = [BACKEND_ADMIN_METHODS];
+const FIELDS = [INPUT_FIELDS];
 
 const SEARCH_ENDPOINT = "AWS_SEARCH_ENDPOINT";
 
