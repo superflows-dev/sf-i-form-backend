a480a60b72e74b4fb188e571cd8a7ed2239d927e
Hrushi M <108924653+superflows-dev@users.noreply.github.com>
2024-08-19 14:05:12 +0530

    Update maskvalue.mjs

--- maskvalue.mjs
+++ maskvalue.mjs
@@ -1,7 +1,7 @@
 export const processMaskValue = (valueStr) => {
     if(valueStr.indexOf('@') >= 0){
         const arrEmail = valueStr.split('@');
-        if(arrEmail[0].length > 1) {
+        if(arrEmail[0].length > 2) {
             arrEmail[0] = (arrEmail[0] + "").substring(0, 2) + "x".repeat(arrEmail[0].length - 3) + (arrEmail[0] + "").substring(arrEmail[0].length - 1, arrEmail[0].length);
         } 
         arrEmail[1] = (arrEmail[1] + "").substring(0, 1) + "x".repeat(arrEmail[1].length - 2) + (arrEmail[1] + "").substring(arrEmail[1].length - 1, arrEmail[1].length);
